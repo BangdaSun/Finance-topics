ad22fe8397a7bb2a8f94c274c427285c4718c1d0
Bangda Sun <bangdasun94@gmail.com>
2017-06-20 11:57:28 -0400

    Create option-payoff&trading-strategies-digaram.R

---
+++ option-payoff&trading-strategies-digaram.R
@@ -0,0 +1,70 @@
+
+# Profit & loss function of call and put options
+
+callOptPnL = function(stock, strike, cost) {
+  # @param stock, stock price
+  # @param strike, strike price
+  # @param cost, constant cost
+  return(max(0, stock - strike) - cost)
+}
+
+putOptPnL = function(stock, strike, cost) {
+  # @param stock, stock price
+  # @param strike, strike price
+  # @param cost, constant cost
+  return(max(0, strike - stock) - cost)
+}
+
+# Profit & loss plot for 4 different type of options
+stockPrice = seq(from = 0, to = 80)
+
+pnlOflongCall = sapply(stockPrice, callOptPnL, strike = 20, cost = 5)
+plot(stockPrice, pnlOflongCall, type = "l"); abline(h = 0, lty = 2)
+
+pnlOflongPut = sapply(stockPrice, putOptPnL, strike = 20, cost = 5)
+plot(stockPrice, pnlOflongPut, type = "l"); abline(h = 0, lty = 2)
+
+pnlOfshortCall = -sapply(stockPrice, callOptPnL, strike = 20, cost = 5)
+plot(stockPrice, pnlOfshortCall, type = "l"); abline(h = 0, lty = 2)
+
+pnlOfshortPut = -sapply(stockPrice, putOptPnL, strike = 20, cost = 5)
+plot(stockPrice, pnlOfshortPut, type = "l"); abline(h = 0, lty = 2)
+
+### Vertical call
+#   long call (low strike) + short call (higher)
+pnlOflongcall_low   = sapply(stockPrice, callOptPnL, strike = 30, cost = 5)
+pnlOfshortcall_high = -sapply(stockPrice, callOptPnL, strike = 50, cost = 5)
+payoff              = pnlOflongcall_low + pnlOfshortcall_high
+plot(stockPrice, payoff, type = "l", main = "vertical call"); abline(h = 0, lty = 2)
+
+### Vertical put
+#   long put (high strike) + short put (lower)
+pnlOflong_put  = sapply(stockPrice, putOptPnL, strike = 50, cost = 5)
+pnlOfshort_put = -sapply(stockPrice, putOptPnL, strike = 30, cost = 5)
+payoff         = pnlOflong_put + pnlOfshort_put
+plot(stockPrice, payoff, type = "l", main = "vertical short"); abline(h = 0, lty = 2)
+
+### Long straddle
+#   long put and call with same strike
+pnlOflong_put  = sapply(stockPrice, putOptPnL, strike = 40, cost = 5)
+pnlOflong_call = sapply(stockPrice, callOptPnL, strike = 40, cost = 5)
+payoff         = pnlOflong_put + pnlOflong_call
+plot(stockPrice, payoff, type = "l", main = "straddle"); abline(h = 0, lty = 2)
+
+### Long strangle
+#   long put (low strike) + long call (higher)
+pnlOflong_put  = sapply(stockPrice, putOptPnL, strike = 40, cost = 5)
+pnlOflong_call = sapply(stockPrice, callOptPnL, strike = 60, cost = 5)
+payoff         = pnlOflong_put + pnlOflong_call
+plot(stockPrice, payoff, type = "l", main = "strangle"); abline(h = 0, lty = 2)
+
+### Butterfly
+#   long call (low strike) + two short call (higher) + long call (highest)
+pnlOflongcall_low  = sapply(stockPrice, callOptPnL, strike = 20, cost = 5)
+pnlOfshortcall_mid = -sapply(stockPrice, callOptPnL, strike = 30, cost = 5)
+pnlOflongcall_high = sapply(stockPrice, callOptPnL, strike = 40, cost = 5)
+totalPayoff        = pnlOflongcall_low + 2 * pnlOfshortcall_mid + pnlOflongcall_high
+plot(stockPrice, totalPayoff, type = "l", main = "butterfly"); abline(h = 0, lty = 2)
+
+### Condor
+#  two different strangles
